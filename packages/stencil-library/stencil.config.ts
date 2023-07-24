4c5ed8a919b45060bdc5559a01b5eb2933ea24e2
Tanner Reits <tanner@ionic.io>
2023-07-24 17:22:34 -0400

    add react output target and build

--- packages/stencil-library/stencil.config.ts
+++ packages/stencil-library/stencil.config.ts
@@ -1,4 +1,5 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'stencil-library',
@@ -7,18 +8,12 @@ export const config: Config = {
       type: 'dist',
       esmLoaderPath: '../loader',
     },
-    {
-      type: 'dist-custom-elements',
-    },
-    {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
-    },
+    reactOutputTarget({
+      componentCorePackage: 'stencil-library',
+      proxiesFile: '../react-library/lib/components/stencil-generated/index.ts',
+    }),
   ],
   testing: {
-    browserHeadless: "new",
+    browserHeadless: 'new',
   },
 };
